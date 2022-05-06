a641a933691b79b170c2f07b23b9d5d25d8232cf
n0099 <n@n0099.net>
2022-05-06 10:57:44 +0800

    + files `tbm_min/Revision/*.proto` to figure out which field is updated with null value * update README.md and protoc.sh to reflect the new dir `tbm_min`

--- protoc.sh
+++ protoc.sh
@@ -1,6 +1,7 @@
-DIR=tbm_cs
+PROTO_PATH=tbm_min
+DIR=tbm_min_cs
 rm -rf $DIR
 mkdir $DIR
-./protoc --csharp_out=$DIR --csharp_opt=base_namespace=TbClient,file_extension=.g.cs --proto_path=tbm $(find tbm -type f -name '*.proto')
+./protoc --csharp_out=$DIR --csharp_opt=base_namespace=TbClient,file_extension=.g.cs --proto_path=$PROTO_PATH $(find $PROTO_PATH -type f -name '*.proto')
 rm -f $DIR.7z
 7z a -mx=9 $DIR.7z ./$DIR/*
