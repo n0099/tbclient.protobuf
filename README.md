3acfaf35cbd3f119cdeb19be74a359915cd19325
n0099 <n@n0099.net>
2022-07-13 16:16:58 +0800

    * update protoc.sh to allow passing args to indicate which lang should be codegen

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 |java|[@Starry-OvO](https://github.com/Starry-OvO) 使用 [dex2jar](https://github.com/pxb1988/dex2jar) 与 [jd-cli](https://github.com/intoolswetrust/jd-cli) 反编译贴吧客户端 apk 后取出 tbclient 根命名空间目录下的源码文件|
 |proto|使用 `squareWireProtoBufJavaGeneratedClassesDecompiler.php` 将这些通过 [square/wire](https://github.com/square/wire) 生成的 java 类源码转换回 .proto 文件|
 |tbm|用于项目 https://github.com/n0099/TiebaMonitor 而遴选并重命名出的部分 .proto 文件|
-|tbm_min|同上但只选取了 tbm 中所引用的字段和 .proto 文件|
-|protoc.sh|将 `tbm_min/*.proto` 文件编译为用于 C# 的实体类，置于 `tbm_min_cs/*.g.cs` 下并打包为 `tbm_min_cs.7z`，依赖于 [protoc](https://github.com/protocolbuffers/protobuf/releases) 和 [7z](https://7-zip.org/download.html) |
+|tbm_min|同上但只选取了 [tbm](https://github.com/n0099/TiebaMonitor) 中所引用的字段和 .proto 文件|
+|protoc.sh|将 `tbm_min/*.proto` 文件codegen为用于 php/C# 的类文件，置于 `tbm_min_{php,cs}` 下并打包为 `tbm_min_{php,cs}.7z`，依赖于 [protoc](https://github.com/protocolbuffers/protobuf/releases) 和 [7z](https://7-zip.org/download.html) |
--- protoc.sh
+++ protoc.sh
@@ -1,7 +1,29 @@
+#!/bin/bash
 PROTO_PATH=tbm_min
-DIR=tbm_min_cs
-rm -rf $DIR
-mkdir $DIR
-./protoc --csharp_out=$DIR --csharp_opt=base_namespace=TbClient,file_extension=.g.cs --proto_path=$PROTO_PATH $(find $PROTO_PATH -type f -name '*.proto')
-rm -f $DIR.7z
-7z a -mx=9 $DIR.7z ./$DIR/*
+USAGE='Usage: passing following possible args: php cs, to generate corresponding codegen files.'
+i=0
+for type in "$@"; do
+    i=$((i+1));
+    echo '    codegen for' $type 'started:';
+    DIR=${PROTO_PATH}_$type;
+    rm -r $DIR
+    mkdir -v $DIR
+    case $type in
+        php)
+            ./protoc --php_out=$DIR --proto_path=$PROTO_PATH $(find $PROTO_PATH -type f -name '*.proto')
+            ;;
+        cs)
+            ./protoc --csharp_out=$DIR --csharp_opt=base_namespace=TbClient,file_extension=.g.cs --proto_path=$PROTO_PATH $(find $PROTO_PATH -type f -name '*.proto')
+            ;;
+        *)
+            echo $USAGE
+            ;;
+    esac
+    rm -vf $DIR.7z
+    7z a -mx=9 $DIR.7z ./$DIR/*
+    echo '    codegen for' $type 'finished.';
+done
+
+if [ $i -eq 0 ]; then
+    echo $USAGE
+fi
