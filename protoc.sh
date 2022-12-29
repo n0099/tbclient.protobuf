f85d19700f9a925eea137d2fd8ce65910d1397bd
n0099 <n@n0099.net>
2022-12-29 16:18:58 +0800

    * add quotes around variables to prevent bash world splitting @ `protoc.sh`

--- protoc.sh
+++ protoc.sh
@@ -4,26 +4,26 @@ USAGE='Usage: passing following possible args: php cs, to generate corresponding
 i=0
 for type in "$@"; do
     i=$((i+1));
-    echo '    codegen for' $type 'started:';
+    echo '    codegen for' "$type" 'started:';
     DIR=${PROTO_PATH}_$type;
-    rm -r $DIR
-    mkdir -v $DIR
+    rm -r "$DIR"
+    mkdir -v "$DIR"
     case $type in
         php)
-            ./protoc --php_out=$DIR --proto_path=$PROTO_PATH $(find $PROTO_PATH -type f -name '*.proto')
+            ./protoc --php_out="$DIR" --proto_path="$PROTO_PATH" "$(find "$PROTO_PATH" -type f -name '*.proto')"
             ;;
         cs)
-            ./protoc --csharp_out=$DIR --csharp_opt=base_namespace=TbClient,file_extension=.g.cs --proto_path=$PROTO_PATH $(find $PROTO_PATH -type f -name '*.proto')
+            ./protoc --csharp_out="$DIR" --csharp_opt=base_namespace=TbClient,file_extension=.g.cs --proto_path="$PROTO_PATH" "$(find "$PROTO_PATH" -type f -name '*.proto')"
             ;;
         *)
-            echo $USAGE
+            echo "$USAGE"
             ;;
     esac
-    rm -vf $DIR.7z
-    7z a -mx=9 $DIR.7z ./$DIR/*
-    echo '    codegen for' $type 'finished.';
+    rm -vf "$DIR".7z
+    7z a -mx=9 "$DIR".7z ./"$DIR"/*
+    echo '    codegen for' "$type" 'finished.';
 done
 
 if [ $i -eq 0 ]; then
-    echo $USAGE
+    echo "$USAGE"
 fi
